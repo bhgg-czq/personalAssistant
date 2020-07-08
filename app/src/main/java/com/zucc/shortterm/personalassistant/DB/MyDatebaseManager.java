ed2ab36beee7349b29b813d38892897682862885
bhgg-czq <1349258926@qq.com>
2020-07-08 15:57:51 +0800

    record update

--- app/src/main/java/com/zucc/shortterm/personalassistant/DB/MyDatebaseManager.java
+++ app/src/main/java/com/zucc/shortterm/personalassistant/DB/MyDatebaseManager.java
@@ -1,7 +1,12 @@
 package com.zucc.shortterm.personalassistant.DB;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.view.View;
+
+import com.zucc.shortterm.personalassistant.Bean.BeanTodo;
+import com.zucc.shortterm.personalassistant.MainActivity;
 
 public class MyDatebaseManager {
     private MyDatebaseHelper helper;
@@ -10,6 +15,19 @@ public class MyDatebaseManager {
     public MyDatebaseManager(Context context){
         helper = new MyDatebaseHelper(context);
         db = helper.getWritableDatabase();
+    }
+
+    public void addTodo(BeanTodo todos) {
+        ContentValues values=new ContentValues();
+        values.put("title",todos.getTitle());
+
+        values.put("remind",todos.getRemind());
+        if (todos.getRemind()!=0){
+            values.put("date",todos.getDate().toString());
+        }
+        values.put("haveDown",0);
+        values.put("PRI",todos.getPRI());
+
     }
 
 
--- app/src/main/java/com/zucc/shortterm/personalassistant/MainActivity.java
+++ app/src/main/java/com/zucc/shortterm/personalassistant/MainActivity.java
@@ -1,5 +1,8 @@
 package com.zucc.shortterm.personalassistant;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
 
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
@@ -8,16 +11,23 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.view.GravityCompat;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
+import com.zucc.shortterm.personalassistant.Bean.BeanTodo;
+import com.zucc.shortterm.personalassistant.DB.MyDatebaseManager;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -25,16 +35,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener,BottomNavigationBar.OnTabSelectedListener{
-        private BottomNavigationBar bottomNavigationBar;
+    implements NavigationView.OnNavigationItemSelectedListener,BottomNavigationBar.OnTabSelectedListener{
+    private BottomNavigationBar bottomNavigationBar;
+    private Dialog dialog,dialog2;
+    private View inflate,inflate2;
+    private EditText nav_dialog_edit;
+    private Button nav_dialog_button;
+    private MyDatebaseManager myDatebaseManager;
+    private TextView textRankH,textRankN,textRankM,textRankL;
+    private ImageView nav_dialog_remind,nav_dialog_pri;
+    private int pri=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
-
+        myDatebaseManager=new MyDatebaseManager(this);
 
 
         setSupportActionBar(toolbar);
@@ -42,10 +66,10 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                showContentDialog();
             }
         });
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -53,13 +77,58 @@ public class MainActivity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
-//        bottomnavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
 
         intiBottomNavigationBar();
 
         bottomNavigationBar.setTabSelectedListener(this);
+
+
+        init();
+
+
     }
+
+    public void init(){
+        dialog2 = new Dialog(this,R.style.PRIDialogStyle);
+        inflate2 = LayoutInflater.from(this).inflate(R.layout.pri_dialog, null);
+        textRankH=inflate2.findViewById(R.id.rankH);
+        textRankM=inflate2.findViewById(R.id.rankM);
+        textRankL=inflate2.findViewById(R.id.rankL);
+        textRankN=inflate2.findViewById(R.id.rankN);
+        dialog2.setContentView(inflate2);
+        //获取当前Activity所在的窗体
+        Window dialogWindow2 = dialog2.getWindow();
+        //设置Dialog从窗体底部弹出
+        dialogWindow2.setGravity( Gravity.BOTTOM);
+        WindowManager.LayoutParams lp = dialogWindow2.getAttributes();
+        lp.y=500;
+        lp.x=-250;
+//       将属性设置给窗体
+        dialogWindow2.setAttributes(lp);
+    }
+
+    public void onClickRankH(View view){
+        nav_dialog_edit.setTextColor(0xffe21e29);
+        pri=1;
+        nav_dialog_pri.setColorFilter(0xffe21e29);
+    }
+    public void onClickRankM(View view){
+        nav_dialog_edit.setTextColor(0xffe5b728);
+        pri=2;
+        nav_dialog_pri.setColorFilter(0xffe5b728);
+    }
+    public void onClickRankL(View view){
+        nav_dialog_edit.setTextColor(0xff667ed6);
+        pri=3;
+        nav_dialog_pri.setColorFilter(0xff667ed6);
+    }
+    public void onClickRankN(View view){
+        nav_dialog_edit.setTextColor(0xff8a8a8a);
+        pri=4;
+        nav_dialog_pri.setColorFilter(0xff8a8a8a);
+    }
+
 
    public void intiBottomNavigationBar(){
        bottomNavigationBar= findViewById(R.id.nav_bottom);
@@ -100,6 +169,88 @@ public class MainActivity extends AppCompatActivity
 
     }
 
+
+    //弹窗
+    private void showContentDialog() {
+        dialog = new Dialog(this,R.style.ActionSheetDialogStyle);
+        //填充对话框的布局
+        inflate = LayoutInflater.from(this).inflate(R.layout.add_info_dialog, null);
+        nav_dialog_edit = (EditText) inflate.findViewById(R.id.nav_dialog_edit);
+        nav_dialog_remind = (ImageView) inflate.findViewById(R.id.nav_dialog_remind);
+        nav_dialog_pri = (ImageView) inflate.findViewById(R.id.nav_dialog_pri);
+        nav_dialog_button = (Button) inflate.findViewById(R.id.nav_dialog_button);
+        //将布局设置给Dialog
+        dialog.setContentView(inflate);
+        //获取当前Activity所在的窗体
+        Window dialogWindow = dialog.getWindow();
+        //设置Dialog从窗体底部弹出
+        dialogWindow.setGravity( Gravity.BOTTOM);
+        //获得窗体的属性
+        WindowManager.LayoutParams lp = dialogWindow.getAttributes();
+        lp.y=20;
+//       将属性设置给窗体
+        dialogWindow.setAttributes(lp);
+        dialog.show();//显示对话框
+
+
+        nav_dialog_edit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (editable.toString().length()==0){
+                    nav_dialog_button.setEnabled(false);
+                    nav_dialog_button.setTextColor(0xff8a8a8a);
+                }
+
+                else{
+                    nav_dialog_button.setTextColor(0xff667ed6);
+                }
+
+            }
+        });
+        nav_dialog_pri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.d("aaaaa","aaa");
+//                System.out.println("fdfdf");
+
+                dialog2.show();
+            }
+        });
+    }
+
+
+
+    //点击确定添加记录
+    public void onClickSure(View v){
+        BeanTodo t=new BeanTodo();
+        if (!nav_dialog_edit.getText().toString().isEmpty())
+        {
+            t.setTitle(nav_dialog_edit.getText().toString());
+            Log.d("a",nav_dialog_edit.getText().toString());
+            t.setPRI(pri);
+            t.setRemind(0);
+            myDatebaseManager.addTodo(t);
+            Log.d("record","添加成功");
+            pri=0;
+        }
+        else{
+            Log.d("a", "error");
+        }
+
+    }
+
+    //侧边抽屉
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
